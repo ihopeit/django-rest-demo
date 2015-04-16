9ebc46a18b3a9ab34574ac0a956ceb8e2daddd92
lvzhaogang <suibwx@163.com>
2015-04-17 06:32:44 +0800

    enable both session authentication and token authentication

--- settings.py
+++ settings.py
@@ -60,8 +60,8 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.DjangoModelPermissions'
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
-    #    'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',  #enable Token authentication
+        'rest_framework.authentication.SessionAuthentication',  #enable session authentication
     ]
 }
 
