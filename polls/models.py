034f0913649facdfbc73fdc56537223645edc5d7
lvzhaogang <suibwx@163.com>
2015-04-17 07:50:05 +0800

    fix database path; comments

--- polls/models.py
+++ polls/models.py
@@ -8,10 +8,13 @@ from django.dispatch import receiver
 from django.conf import settings
 from rest_framework.authtoken.models import Token
 
+## If you want every user to have an automatically generated Token, you can simply catch the User's post_save signal.
+## Actually, don't need it most of the times. May use it when we need token for a user after register (such as api access, we may need to send api to the user)
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_auth_token(sender, instance=None, created=False, **kwargs):
     if created:
         Token.objects.create(user=instance)
+
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
